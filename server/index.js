01245d6a5db0acfeecce1cafaf338f1f68aa1486
dizwe <dizwe2716@gmail.com>
2018-05-24 10:34:05 +0900

    lambda 성공한 function

--- server/index.js
+++ server/index.js
@@ -1,39 +1,22 @@
 console.log('Loading function');
-
 var mongoose = require('mongoose');
 var Bunch = require('./models/bunch.js');
 var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-  console.log("Connected to mogod server");
-});
-//test db로 접속해라
-mongoose.connect(process.env.db_url);
-
+console.log(process.env.db_url);
 
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
+    //test db로 접속해라
+    mongoose.connect(process.env.db_url);
     const operation = event.http_method;
-
-    switch (operation) {
-        // case 'POST':
-        //     var replaced_hi = event.bunch.replace(/'/g,'"');
-        //     payload.Item = JSON.parse(replaced_hi);
-        //     return dynamo.putItem(payload, callback);
-        //     // break;
-        case 'GET':
-            Bunch.find({"seen":{$lte:3}})
-            .sort({"written_date":1})
-            .limit(30)
-            .exec(function(err, bunch) {
-                if(err)
-              return callback(null,bunch);
-            });
-            break;
-        // case 'PUT':
-        //     dynamo.updateItem(payload, callback);
-        //     break;
-        default:
-            callback(new Error(`Unrecognized operation "${operation}"`));
-    }
+    console.log(operation);
+    db.once('open', function(){
+       Bunch.find({"seen":{$lte:3}})
+        .sort({"written_date":1})
+        .limit(30)
+        .exec(function(err, bunch) {
+            mongoose.connection.close();
+            if(err) console.log(err);
+            else callback(null,bunch);
+        });
+    });
 };
