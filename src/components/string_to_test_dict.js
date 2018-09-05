347e4aefc10ab1643ebb1d11040edf1cee7604f3
dizwe <dizwe2716@gmail.com>
2018-09-05 23:53:53 +0900

    RESOLVE string_to_test_dict |가 없으면 word와 etc둘다 단어가 적히는 문제 해결(slice undefined 때문),

--- src/components/string_to_test_dict.js
+++ src/components/string_to_test_dict.js
@@ -1,10 +1,10 @@
 module.exports = function(test_string){
   // 한 번치 분리
   let _splited_string = test_string.split('*');
-  
+
   // 아니면 한글이나 영어나 중국어 있으면 바꿔라 이런건 안되려나?
   // 뒤에 적힌거를 포함하지 않는 친구가 있을 때 && 5글자 이상일때이걸 해라
-  let bunch = _splited_string.filter(x=>x.search(/[^123| \t↵\n]/)!=-1&&x.length>=5);
+  let bunch = _splited_string.filter(x=>x.search(/[^123|\:> \t↵\n]/)!=-1&&x.length>=5);
 
   // 번치에서 단어 분리
   // 첫번째로 나오는 | 만 파싱하기 위해 split 말고 indexOf를 사용함
@@ -15,9 +15,12 @@ module.exports = function(test_string){
 
   for(let i=0; i<bunch.length; i++){
     // :를 slice로 생각해야 하는군... 쳇
-    word = bunch[i].slice(0,_spliting_bunch_index[i]);
-    let etc = bunch[i].slice(_spliting_bunch_index[i]+1);
-    let splited_etc = etc.split(/\n/g);
+    // |를 아예 안적은
+    word = bunch[i].slice(0,_spliting_bunch_index[i]?_spliting_bunch_index[i]:bunch.length);
+   // |를 안적은 경우에는 etc를 아예 빈칸으로 둠.
+   let etc = bunch[i].slice(_spliting_bunch_index[i]?_spliting_bunch_index[i]+1:bunch[i].length);
+
+    let splited_etc = etc.split(/:>/g);
     mean = splited_etc[0];
     cf = splited_etc.filter(x=>x.indexOf('c.f.')!=-1).map(x=>x.replace('c.f.',''));
     eg = splited_etc.filter(x=>x.indexOf('e.g.')!=-1).map(x=>x.replace('e.g.',''));
